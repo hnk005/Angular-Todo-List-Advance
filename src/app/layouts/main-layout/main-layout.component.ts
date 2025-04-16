7ed5f402e6c33bfa0da1b6f051ab0a91796589c6
hnk005 <huynhnamkha512020@gmail.com>
2025-04-16 10:13:49 +0700

    complete create project structure

---
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  templateUrl: './main-layout.component.html',
+})
+export class MainLayoutComponent {}
