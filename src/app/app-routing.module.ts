16b7ca5688b7858c20a3bba708941bb974ba13e8
hnk005 <huynhnamkha512020@gmail.com>
2025-04-17 20:51:07 +0700

    complete basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainLayoutModule } from './core/layouts/main-layout/main-layout.module';
 import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
+import { TaskTodayComponent } from './modules/todo/pages/task-today/task-today.component';
+import { MainLayoutModule } from './core/layouts/main-layout/main-layout.module';
 
 const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-      
-    ]
+      { path: '', redirectTo: 'task-today', pathMatch: 'full' },
+      {
+        path: 'task-today',
+        component: TaskTodayComponent,
+      },
+    ],
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), MainLayoutModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TaskTodayComponent } from './pages/task-today/task-today.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TaskTodayComponent
+  ],
   imports: [
     CommonModule
   ]
